30954fe9eae666e94f56ad73662070da596e27ed
Pritam <Pritam.lhs@gmail.com>
2026-03-10 02:15:27 +0530

    Practicing Numpy Modules

---
+++ Python/numpy_example.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+my_list = [1, 2, 3]
+print(np.array(my_list))
+
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from learnings-pandas-numpy!")
+
+
+if __name__ == "__main__":
+    main()
