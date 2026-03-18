0396616dafa885d716caacdf707bd5e03191df33
Pritam <Pritam.lhs@gmail.com>
2026-03-18 22:07:35 +0530

    conditional filtering

---
+++ Python/dataframes_conditional_filtering.py
@@ -0,0 +1,1 @@
+# we will learn about conditional filtering in this python file. We will learn how to filter data in a dataframe based on certain conditions. This is a very common operation in data analysis and it is used to extract specific rows from a dataframe that meet certain criteria. We will also learn how to use multiple conditions for filtering data in a dataframe.
