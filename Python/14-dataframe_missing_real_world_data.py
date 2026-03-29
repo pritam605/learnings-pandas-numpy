af55b264ad016f642919a24a70be554166eb9d19
Pritam <Pritam.lhs@gmail.com>
2026-03-29 11:49:36 +0530

    handling null values

--- Python/14-dataframe_missing_real_world_data.py
+++ Python/14-dataframe_missing_real_world_data.py
@@ -21,16 +21,45 @@ print(df_orders.head(5))
 
 # np.nan is not equal to np.nan so np.nan == np.nan returns false
 
-# HEnce do not use equality, rather write it like "np.nan is np.nan" this way it returns true.
+# Hence do not use equality, rather write it like "np.nan is np.nan" this way it returns true.
 #e.g.,
 import numpy as np
 myvar = np.nan
 
 print(myvar is np.nan)
 
+# Handling missing data - 
+# With pandas we can drop the entire row or an entire column that is missing based on conditions. This cna lead to missing of useful data and limits training data. 
+
+#We can impute the missing data as well with statistical estimation. 
+# We may end up dropping imporant missing data or columns. 
 # Let's read a file with empty values. 
 import pandas as pd
 #pd.read_csv
-# We will then deal with the missing values.
-# 
-# Code starts from below. 
+
+df_orders = pd.read_csv(r'files/orders.csv')
+print(df_orders.head(5))
+
+print(
+    
+    df_orders.isnull() #df_orders.notnull()
+      
+      )
+
+print(df_orders[df_orders['Category'].notnull()]) #Applied conditional filtering to select not null cases from df_orders. 
+
+print(df_orders[df_orders['Category'].isnull()]) #Returns blank because all are filled. no null cases
+
+print(df_orders[df_orders['Category'].isnull() & df_orders['Country'].isnull()])
+
+#Handling missing data via Threshold
+# thres = xx where xx means the row should have atleast that many non null values
+df_orders(thres=5) # Meaning drop rows where the orders dataframe doe snot contain atleast 5 non null values. Note that this is doing row wise filtering. 
+
+df_orders.dropna(axis=1) #axis 1 means columns so what this tells is every column that has a missing value drop them.
+
+df_orders.dropna(axis=0) #axis 0 means rows so what this tells is every rows that has a missing value drop them.
+
+df_orders.dropna(subset = ['last_name']) #Only considers certain column to look for missing values in those columns only. 
+
+df_orders.dropna(thres = 4, subset = ['last_name']) # This way we can keep a combination of threshold function along with subset functions. So, any row where number of non-null vavlues are more than 4, it is going to drop it off only if last_name is null for that row. 
